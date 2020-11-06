93128c999f64781c1cbf6204635fd8200934273f
Russell <35252806+RussellAbraham@users.noreply.github.com>
2020-11-05 16:15:37 -0800

    Add files via upload

---
+++ tools/build.js
@@ -0,0 +1,12 @@
+{
+    "appDir": "../www",
+    "mainConfigFile": "../www/assets.js",
+    "dir": "../www-built",
+    // List the modules that will be optimized.
+    "modules": [
+        {
+            // module names are relative to baseUrl
+            "name": "assets"
+        }
+    ]
+}
