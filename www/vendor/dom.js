b8a045b8e8398290dba223fdd5430d3c4b584a04
Russell <35252806+RussellAbraham@users.noreply.github.com>
2020-11-18 09:03:36 -0800

    Update dom.js

--- www/vendor/dom.js
+++ www/vendor/dom.js
@@ -1,26 +1,22 @@
 /* DOM Library */
 (function () {
+
     var root = this;
+
     var $ = function (obj) {
         if (obj instanceof $) return obj;
         if (!(this instanceof $)) return new $(obj);
         this._wrapped = obj;
     };
-    if (typeof exports !== 'undefined') {
-        if (typeof module !== 'undefined' && module.exports) {
-            exports = module.exports = $;
-        }
-        exports.$ = $;
-    } else {
-        root.$ = $;
-    }
-    $.VERSION = '0.0.1';
-    DocumentFragment.prototype.append = function (element) {
-        return this.appendChild(element);
-    }
-    DocumentFragment.prototype.render = function (target) {
-        return target.appendChild(this);
-    }
+
+    DocumentFragment.prototype.append = function (element, scope) {
+        return (this || scope).appendChild(element);
+    };
+
+    DocumentFragment.prototype.render = function (target, scope) {
+        return target.appendChild(this || scope);
+    };
+
     const fragment = new DocumentFragment();
 
     function extend(obj) {
@@ -31,20 +27,27 @@
         });
         return obj;
     };
+
     extend($, fragment);
+
     $.id = document.getElementById.bind(document);
+
     $.qs = function (selector, scope) {
         return (scope || document).querySelector(selector);
     };
+
     $.qsa = function (selector, scope) {
         return (scope || document).querySelectorAll(selector);
     };
+
     $.closest = function (target, selector) {
         return target.closest(selector);
-    }
+    };
+
     $.listen = function (target, type, callback, useCapture) {
         target.addEventListener(type, callback, !!useCapture);
     };
+
     $.delegate = function (target, selector, type, handler) {
         function dispatchEvent(event) {
             var targetElement = event.target;
@@ -68,10 +71,6 @@
         return $.parent(element.parentNode, tagName);
     };
 
-    $.closest = function (selector, element) {
-        return $.qs(selector).closest(element); //.qs();
-    };
-
     $.create = function (target, element, options) {
         options = (options || {});
         const parent = document.createElement(element);
@@ -92,12 +91,69 @@
         return fragment.render(target);
     };
 
+    $.ajax = function (options) {
+        options = options || {};
+        var type = options.type || 'GET';
+        var url = options.url;
+        var processData = options.processData === undefined ? true : !!options.processData;
+        var contentType = options.contentType || 'application/x-www-form-urlencoded; charset=UTF-8';
+        var data = options.data;
+        if (processData && typeof data === 'object') {
+            var params = Object.keys(data).map(function (prop) {
+                return encodeURIComponent(prop) + '=' + encodeURIComponent(data[prop]);
+            });
+            data = params.join('&');
+        }
+        if (data && (type === 'GET' || type === 'HEAD')) {
+            url += (url.indexOf('?') === -1 ? '?' : '&') + data;
+            data = undefined;
+        }
+        var xhr = new XMLHttpRequest();
+        xhr.open(type, url, true);
+        xhr.setRequestHeader('Content-Type', contentType);
+        if (options.beforeSend) options.beforeSend(xhr);
+        xhr.onload = function () {
+            var error = false;
+            var content = xhr.responseText;
+            if (options.dataType === 'json') {
+                try {
+                    content = JSON.parse(content);
+                } catch (e) {
+                    error = true
+                }
+            }
+            if (!error && (xhr.status >= 200 && xhr.status < 300)) {
+                if (options.success) options.success(content, xhr.statusText, xhr);
+            } else {
+                if (options.error) options.error(xhr);
+            }
+        }.bind(this);
+        xhr.onerror = xhr.onabort = function () {
+            if (options.error) options.error(xhr);
+        };
+        xhr.send(data);
+        return xhr;
+    };
+
     $.prototype.wrapped = function () {
         return this._wrapped;
     };
+
+    $.VERSION = '0.0.3';
+
+    if (typeof exports !== 'undefined') {
+        if (typeof module !== 'undefined' && module.exports) {
+            exports = module.exports = $;
+        }
+        exports.$ = $;
+    } else {
+        root.$ = $;
+    }
+
     if (typeof define === 'function' && define.amd) {
         define('dom', [], function () {
             return $;
         });
     }
+
 }.call(this));
