3170c0d20af558b85d9fdbf63c99bd0ce8b455d6
Russell <35252806+RussellAbraham@users.noreply.github.com>
2020-12-07 22:19:12 -0800

    Update assets.js

--- www/assets.js
+++ www/assets.js
@@ -2,15 +2,12 @@
 requirejs.config({
     baseUrl: 'vendor',
     paths: {
-        assets : '../assets',    
-        cdn : 'https://assets.codepen.io/1674766',
-        s3 : 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/1674766',
+        assets : '../assets',
+        link : 'link',
+        text : 'text'
     }
 });
-requirejs(['util', 'dom', 'base', 'localforage', 'sync'], function(_, $, Base, localforage, sync){
-    if(typeof window !== 'undefined'){
-        console.log(_.prototype);
-        console.log($.prototype)
-        console.log(Base);
-    }
+
+requirejs(['assets/js/lib/main', 'text!assets/js/lib/template.html'], function(){
+    
 });
