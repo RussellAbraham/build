d8cc1dda89338f140abf84db05f624e8e367250d
Russell <35252806+RussellAbraham@users.noreply.github.com>
2020-12-22 21:07:56 -0800

    Update collection.js

--- www/assets/js/collections/collection.js
+++ www/assets/js/collections/collection.js
@@ -1,26 +1,30 @@
-define(['backboneLocalforage', 'assets/js/models/model' ],function (Sync, Model) {    
-    
-    var Collection = Backbone.Collection.extend({
-        model : Model,
-        sync : Backbone.localforage.sync('graph-collection'),
-		activated : function () {
-			return this.where({ active : true });
+define(['backboneLocalforage', 'assets/js/models/model'], function (Sync, Model) {
+
+	var Collection = Backbone.Collection.extend({
+		model: Model,
+		sync: Backbone.localforage.sync('graph-collection'),
+		activated: function () {
+			return this.where({
+				active: true
+			});
 		},
-		deactivated : function () {
-			return this.where({ active : false });
+		deactivated: function () {
+			return this.where({
+				active: false
+			});
 		},
-		nextOrder : function () {
+		nextOrder: function () {
 			return this.length ? this.last().get('order') + 1 : 1;
 		},
 		comparator: 'order',
-		clear : function(){
-			while(this.models.length){
+		clear: function () {
+			while (this.models.length) {
 				this.models[0].cancel();
 				this.models[0].destroy();
 			}
 		}
-    });
-    
-    return Collection;
-    
+	});
+
+	return Collection;
+
 });
