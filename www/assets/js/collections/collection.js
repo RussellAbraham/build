aa7c00b89c184e0db0c4aeb31784ea11e181ac64
Russell <35252806+RussellAbraham@users.noreply.github.com>
2020-12-28 13:46:01 -0800

    Update

--- www/assets/js/collections/collection.js
+++ www/assets/js/collections/collection.js
@@ -1,30 +1,69 @@
 define(['backboneLocalforage', 'assets/js/models/model'], function (Sync, Model) {
 
-	var Collection = Backbone.Collection.extend({
-		model: Model,
-		sync: Backbone.localforage.sync('graph-collection'),
-		activated: function () {
-			return this.where({
-				active: true
-			});
-		},
-		deactivated: function () {
-			return this.where({
-				active: false
-			});
-		},
-		nextOrder: function () {
-			return this.length ? this.last().get('order') + 1 : 1;
-		},
-		comparator: 'order',
-		clear: function () {
-			while (this.models.length) {
-				this.models[0].cancel();
-				this.models[0].destroy();
-			}
-		}
-	});
+    var Base = Backbone.Collection.extend({
+        setDriver: function () {
+            //''.toUpperCase();
+        },
+        drop: function () {
+            while (this.length) {
+                this.models[0].destroy();
+            }
+        },
+        import: function (json) {
+            var self = this;
+            _.each(json, function (obj, idx, json) {
+                return self.create(obj);
+            });
+        },
+        export: function () {
+            var self = this;
+            self.anchor = document.createElement("a");
+            self.anchor.download = "export-" + Date.now() + ".json";
+            self.anchor.href = window.URL.createObjectURL(
+                new Blob(
+                    [JSON.stringify(self.toJSON(), null, 2)], {
+                        type: "application/json"
+                    }
+                )
+            );
+            self.anchor.click();
+            delete self.anchor;
+            window.URL.revokeObjectURL(self.url);
+        }
+    });
 
-	return Collection;
+    var Repos = Base.extend({
+        initialize: function () {
+            this.fetch();
+        },
+        url: "https://api.github.com/users/autoraidapi/repos"
+    });
+
+    var Collection = Base.extend({
+        model: Model,
+        sync: Backbone.localforage.sync('graph-collection'),
+        activated: function () {
+            return this.where({
+                active: true
+            });
+        },
+        deactivated: function () {
+            return this.where({
+                active: false
+            });
+        },
+        nextOrder: function () {
+            return this.length ? this.last().get('order') + 1 : 1;
+        },
+        comparator: 'order',
+        clear: function () {
+            while (this.models.length) {
+                this.models[0].cancel();
+                this.models[0].destroy();
+            }
+        }
+    });
+
+    return Collection;
 
 });
